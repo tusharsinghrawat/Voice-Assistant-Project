ceb800fde968649e992af5bcac0fc49c0c2d81d1
Tushar <tusharsinghrawat45@gmail.com>
2025-11-08 19:49:48 +0530

    Initial commit – Voice Assistant Project upload

---
+++ jarvis..py
@@ -0,0 +1,92 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('volume', 1.0)  # Max volume
+
+def speak(audio):
+    print(f"Speaking: {audio}")
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning Sir!")
+ 
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon Sir!")
+
+    else:
+        speak("Good Evening Sir!")
+    
+    speak("I am Jarvis. Please tell me how may I help you")
+
+def takeCommand(): #It takes microphone input from the user and returns string output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...") 
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+   
+
+if __name__ == "__main__":
+    wishMe()
+#  while True:
+    if 1:
+      query = takeCommand().lower()
+      
+
+    # Logic for executing tasks based on query
+    if 'wikipedia' in query:
+        speak('Searching Wikipedia...')
+        query = query.replace("wikipedia", "")
+        results = wikipedia.summary(query, sentences=2)
+        speak("According to Wikipedia")
+        print(results)
+        speak(results)
+
+    elif 'open youtube' in query:
+        webbrowser.open("youtube.com")
+
+    elif 'open google' in query:
+        webbrowser.open("google.com")
+
+    elif 'open stackoverflow' in query:
+        webbrowser.open("stackoverflow.com")
+
+    elif 'open amazon' in query:
+        webbrowser.open("amazon.com")
+
+    elif 'open flipkart' in query:
+        webbrowser.open("flipkart.com")
+
+    elif 'the time' in query:
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Sir, the time is {strTime}")
+    
+    elif 'open code' in query:
+        codePath = "C:\\Microsoft VS Code\\Code.exe"
+        os.startfile(codePath)
+
+    elif 'exit' in query or 'quit' in query:
+        speak("Goodbye Sir!")
+       
